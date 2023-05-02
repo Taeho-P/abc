87749e4e34005b7f035ee796742caaa667773665
Class01 <Class01@DESKTOP-5F4QJGN>
2023-05-02 18:19:22 +0900

    2023.05.02 수업종료

---
+++ baekjoon/src/p10998/Main.java
@@ -0,0 +1,17 @@
+package p10998;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int iA = sc.nextInt();
+		int iB = sc.nextInt();
+		
+		System.out.println(iA * iB);
+
+	}
+
+}
