f380eb1fc48d04afbe3a9e8e105849346416dd8b
goekd <goekd@Taeho>
2023-05-03 18:10:33 +0900

    2023.05.03 수업종료

---
+++ Baekjoon/src/p1000/Main.java
@@ -0,0 +1,19 @@
+package p1000;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int iA = sc.nextInt();
+		int iB = sc.nextInt();
+		
+		sc.close();
+		
+		System.out.print(iA + iB);
+
+	}
+
+}
